a299f774b695e657d6794da2fc091b279d7c0753
FrankSchmutz <frank.schmutz1994@gmail.com>
2019-11-27 12:37:34 +0100

    full script

--- train_test_model.sh
+++ train_test_model.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "training with DSsize = 16"
-python3 train.py --dataset_size=16 --epochs=40 --milestone=20
+python3 train.py --preprocess=True --dataset_size=16 --epochs=40 --milestone=20
 
 for i in {2..400}
 do
   DSsize=$(($i*16))
   echo "training with DSsize = $DSsize"
-  python3 train.py --dataset_size=$DSsize --epochs=30 --milestone=20
+  python3 train.py --dataset_size=$DSsize --epochs=40 --milestone=20
 done
 
 python3 test.py
