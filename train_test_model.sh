b01ada5f08824abea7464c80efef5d572e979192
FrankSchmutz <frank.schmutz1994@gmail.com>
2019-11-27 19:24:46 +0100

    better script

--- train_test_model.sh
+++ train_test_model.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 echo "training with DSsize = 16"
-python3 train.py --preprocess=True --dataset_size=16 --epochs=40 --milestone=20
+if [ "$1" == "create_dataset" ]; then
+  python3 train.py --preprocess=True --dataset_size=16 --epochs=40 --milestone=20
+else
+  python3 train.py --dataset_size=16 --epochs=40 --milestone=20
+fi
 
 for i in {2..400}
 do
