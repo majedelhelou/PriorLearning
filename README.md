09315831dcb268c9bc01d25062b5c32802528217
FrankSchmutz <frank.schmutz1994@gmail.com>
2020-01-09 23:11:36 +0100

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # PriorLearning
+
+### 1. Dependencies
+* Numpy
+* Matplotlib
+* OpenCV for Python
+* [HDF5 for Python](http://www.h5py.org/)
+* PyTorch
+
+### 2. Reproduce results
+To train the models you have two possibilities:
+- Run the 'train.py' directly with the desired parameters. The list of possible parameters is printed when the file is run with the '--help' argument. Note the the only two supported optimizers are 'SGD' and 'Adam', and the '--augmentation' argument supports either 'no' data augmentation, 'standard' or 'vae'. We recommend writing a script to automate this process when running on multiple dataset sizes.
+- Run the 'train_test_model.sh' script to automatically get the results for the same dataset sizes we used. This scripts supports the following arguments: '-c' which should be set to 'true' to create the h5py dataset; '-s' to set the seed; '-o' to specify the optimizer; '-lr' to specify the learning rate; '-b' to specify the batch size; '-k' to specify the sigma parameter of the gaussian kernel used to blur the images; '-d' to specify the number of layers in the model; '-a' to specify the type of data augmentation. Note that if script displays the 'bad interpreter ...' message you can run this command to fix the problem: `sed -i 's/\r//' train_test_model.sh`.
+
+### 3. Visualize the results
+You can use the 'Results' notebook to visualize the results. You need to have the 'logs' folder at the same location as your notebook for it to work. The code to generate all the graphs used in the report is on the notebook and can be used as is as long as you have run the code with the parameters you want to visualize.
+
+If you want to run all the experiments you can simply run the 'experiments.sh' script but the running time will be very long!
--- experiments.sh
+++ experiments.sh
@@ -1,52 +1,50 @@
 #!/bin/bash
 
-if [ "$1" == "1" ]; then
-  ./train_test_model.sh -o SGD -lr 1e-3
-  ./train_test_model.sh -o SGD -lr 1e-2 -b 32
-  ./train_test_model.sh -o SGD -lr 1e-4 -b 32
-  ./train_test_model.sh -o SGD -lr 1e-2 -b 64
-  ./train_test_model.sh -o SGD -lr 1e-4 -b 64
-  ./train_test_model.sh -o SGD -lr 1e-2 -b 128
-  ./train_test_model.sh -o SGD -lr 1e-4 -b 128
-  ./train_test_model.sh -o SGD -k 1
-  ./train_test_model.sh -o SGD -k 2
-  ./train_test_model.sh -o SGD -k 4
-  ./train_test_model.sh -o SGD -k 5
-fi
+./train_test_model.sh -o SGD -lr 1e-3
+./train_test_model.sh -o SGD -lr 1e-2 -b 32
+./train_test_model.sh -o SGD -lr 1e-4 -b 32
+./train_test_model.sh -o SGD -lr 1e-2 -b 64
+./train_test_model.sh -o SGD -lr 1e-4 -b 64
+./train_test_model.sh -o SGD -lr 1e-2 -b 128
+./train_test_model.sh -o SGD -lr 1e-4 -b 128
+./train_test_model.sh -o SGD -k 1
+./train_test_model.sh -o SGD -k 2
+./train_test_model.sh -o SGD -k 3
+./train_test_model.sh -o SGD -k 4
+./train_test_model.sh -o SGD -k 5
 
-if [ "$1" == "2" ]; then
-  ./train_test_model.sh -o Adam -lr 1e-2 -b 32
-  ./train_test_model.sh -o Adam -lr 1e-4 -b 32
-  ./train_test_model.sh -o Adam -lr 1e-2 -b 64
-  ./train_test_model.sh -o Adam -lr 1e-4 -b 64
-  ./train_test_model.sh -o Adam -lr 1e-2 -b 128
-  ./train_test_model.sh -o Adam -lr 1e-4 -b 128
-  ./train_test_model.sh -o Adam -k 1
-  ./train_test_model.sh -o Adam -k 2
-  ./train_test_model.sh -o Adam -k 4
-  ./train_test_model.sh -o Adam -k 5
-fi
+./train_test_model.sh -o SGD -d 5 -lr 1e-2
+./train_test_model.sh -o SGD -d 10 -lr 1e-2
+./train_test_model.sh -o SGD -d 15 -lr 1e-2
+./train_test_model.sh -o SGD -d 5 -lr 1e-3
+./train_test_model.sh -o SGD -d 10 -lr 1e-3
+./train_test_model.sh -o SGD -d 15 -lr 1e-3
+./train_test_model.sh -o SGD -d 5 -lr 1e-4
+./train_test_model.sh -o SGD -d 10 -lr 1e-4
+./train_test_model.sh -o SGD -d 15 -lr 1e-4
 
-if [ "$1" == "3" ]; then
-  ./train_test_model.sh -o SGD -d 5 -lr 1e-2
-  ./train_test_model.sh -o SGD -d 10 -lr 1e-2
-  ./train_test_model.sh -o SGD -d 15 -lr 1e-2
-  ./train_test_model.sh -o SGD -d 5 -lr 1e-3
-  ./train_test_model.sh -o SGD -d 10 -lr 1e-3
-  ./train_test_model.sh -o SGD -d 15 -lr 1e-3
-  ./train_test_model.sh -o SGD -d 5 -lr 1e-4
-  ./train_test_model.sh -o SGD -d 10 -lr 1e-4
-  ./train_test_model.sh -o SGD -d 15 -lr 1e-4
-fi
+./train_test_model.sh -o Adam -lr 1e-3
+./train_test_model.sh -o Adam -lr 1e-2 -b 32
+./train_test_model.sh -o Adam -lr 1e-4 -b 32
+./train_test_model.sh -o Adam -lr 1e-2 -b 64
+./train_test_model.sh -o Adam -lr 1e-4 -b 64
+./train_test_model.sh -o Adam -lr 1e-2 -b 128
+./train_test_model.sh -o Adam -lr 1e-4 -b 128
+./train_test_model.sh -o Adam -k 1
+./train_test_model.sh -o Adam -k 2
+./train_test_model.sh -o Adam -k 3
+./train_test_model.sh -o Adam -k 4
+./train_test_model.sh -o Adam -k 5
 
-if [ "$1" == "4" ]; then
-  ./train_test_model.sh -o Adam -d 5 -lr 1e-2
-  ./train_test_model.sh -o Adam -d 10 -lr 1e-2
-  ./train_test_model.sh -o Adam -d 15 -lr 1e-2
-  ./train_test_model.sh -o Adam -d 5 -lr 1e-3
-  ./train_test_model.sh -o Adam -d 10 -lr 1e-3
-  ./train_test_model.sh -o Adam -d 15 -lr 1e-3
-  ./train_test_model.sh -o Adam -d 5 -lr 1e-4
-  ./train_test_model.sh -o Adam -d 10 -lr 1e-4
-  ./train_test_model.sh -o Adam -d 15 -lr 1e-4
-fi
+./train_test_model.sh -o Adam -d 5 -lr 1e-2
+./train_test_model.sh -o Adam -d 10 -lr 1e-2
+./train_test_model.sh -o Adam -d 15 -lr 1e-2
+./train_test_model.sh -o Adam -d 5 -lr 1e-3
+./train_test_model.sh -o Adam -d 10 -lr 1e-3
+./train_test_model.sh -o Adam -d 15 -lr 1e-3
+./train_test_model.sh -o Adam -d 5 -lr 1e-4
+./train_test_model.sh -o Adam -d 10 -lr 1e-4
+./train_test_model.sh -o Adam -d 15 -lr 1e-4
+
+./train_test_model.sh -o Adam -a standard
+./train_test_model.sh -o Adam -a vae
